ec878ca16f1ef8817dbedbd1b9e8e5ba95555f1f
gtsiolis <tsiolis.g@gmail.com>
2020-11-01 12:54:07 +0000

    [page] Improve header actions layout on small viewports

--- page/src/components/Header.tsx
+++ page/src/components/Header.tsx
@@ -7,8 +7,10 @@ const Header: React.SFC<{}> = (props) => (
         <span className="logo"><img src={logo} alt="" height="200px" /></span>
         <h1>Just Kubernetes Native CI</h1>
         <p>Because your job is hard enough.</p>
-        <a class="button primary" href="#getting-started">Getting started</a>
-        <a class="button icon solid fa-github source" target="_blank" href="https://github.com/csweichel/werft">View on GitHub</a>
+        <ul class="actions">
+            <li><a class="button primary" href="#getting-started">Getting started</a></li>
+            <li><a class="button icon solid fa-github" target="_blank" href="https://github.com/csweichel/werft">View on GitHub</a></li>
+        </ul>
     </header>
 )
 
